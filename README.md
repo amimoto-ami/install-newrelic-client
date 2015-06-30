e3e58ea6686f7770fc9b145ebcc7148e0cc825af
wokamoto <wokamoto1973@gmail.com>
2015-06-30 11:16:49 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Creating Snaphosts for AMIMOTO
+
+## What's AMIMOTO
+
+[AMIMOTO is a High Performance WordPress Cloud Hosting on Amazon Web Services.](http://amimoto-ami.com/)
+
+## Install NewRelic Client for AMIMOTO
+
+AMIMOTO install NewRelic client you need.
+
+You should just run following:
+
+```
+LICENSE_KEY={YOUR_NEWRELIC_LICENCE_KEY_HERE} sudo base -c "$(curl -sSL https://raw.githubusercontent.com/amimoto-ami/install-newrelic-client
+/master/newrelic_install.sh)"
+```
---
+++ newrelic_install.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+NR_INSTALL_KEY=${LICENSE_KEY}
+if [ "${NR_INSTALL_KEY}" = "" ]; then
+  echo "Please set environment variable 'LICENSE_KEY'."
+  exit 1
+fi
+
+# install newrelic sysmond
+rpm -Uvh https://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarch.rpm
+yum -y install newrelic-sysmond
+nrsysmond-config --set license_key=${NR_INSTALL_KEY}
+service newrelic-sysmond start
+chkconfig newrelic-sysmond on
+
+# install newrelic php
+NR_INSTALL_SILENT=1
+yum -y install newrelic-php5
+newrelic-install install
+cp -p /etc/newrelic/newrelic.cfg.template /etc/newrelic/newrelic.cfg
+sed -i -e "s/newrelic.appname = \"PHP Application\"/newrelic.appname = \"${INSTANCE_ID} PHP Application\"/g" /etc/php.d/newrelic.ini
+chkconfig newrelic-daemon on
+service php-fpm restart
+service newrelic-daemon restart
+
+# install mysql plugin
+PREFIX="/opt/newrelic-npi"
+mysql -u root -e \
+  "CREATE USER newrelic@localhost IDENTIFIED BY '${INSTANCE_ID}'; GRANT PROCESS,REPLICATION CLIENT ON *.* TO newrelic@localhost;"
+yes | LICENSE_KEY=${NR_INSTALL_KEY} bash -c "$(curl -sSL https://download.newrelic.com/npi/release/install-npi-linux-redhat-x64.sh)"
+cd ${PREFIX}
+./npi config set license_key ${NR_INSTALL_KEY}
+./npi install nrmysql -y -q -n
+PLUGIN_JSON=${PREFIX}/plugins/com.newrelic.plugins.mysql.instance/newrelic_mysql_plugin-2.0.0/config/plugin.json
+sed -i -e "s/Localhost/${INSTANCE_ID} MySQL/g" ${PLUGIN_JSON}
+sed -i -e "s/USER_NAME_HERE/newrelic/g" ${PLUGIN_JSON}
+sed -i -e "s/USER_PASSWD_HERE/${INSTANCE_ID}/g" ${PLUGIN_JSON}
+chkconfig newrelic_plugin_com.newrelic.plugins.mysql.instance on
+service newrelic_plugin_com.newrelic.plugins.mysql.instance restart
+
+# monit setting
+echo 'check process newrelic-sysmond
+  with pidfile /var/run/newrelic/nrsysmond.pid
+  start program = "/sbin/service newrelic-sysmond start"
+  stop program = "/sbin/service newrelic-sysmond stop"
+
+check process newrelic-daemon
+  with pidfile /var/run/newrelic-daemon.pid
+  start program = "/sbin/service newrelic-daemon start"
+  stop program = "/sbin/service newrelic-daemon stop"
+
+check process newrelic_plugin_com.newrelic.plugins.mysql.instance
+  with pidfile /var/run/newrelic_plugin_com.newrelic.plugins.mysql.instance.pid
+  start program = "/sbin/service newrelic_plugin_com.newrelic.plugins.mysql.instance start"
+  stop program = "/sbin/service newrelic_plugin_com.newrelic.plugins.mysql.instance stop"' > /etc/monit.d/newrelic
+service monit restart
